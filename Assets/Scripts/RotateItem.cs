3baacccba67197d8964cbcd82ed6bad5dca87020
ArmZaDev <arztv001@gmail.com>
2022-06-14 17:51:34 +0700

    Add tag and Group Item

---
+++ Assets/Scripts/RotateItem.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateItem : MonoBehaviour
+{
+
+    public float speed = 100f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(speed * Time.deltaTime, 0f, 0f);
+    }
+}
